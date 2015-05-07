a4bcfccf524919234c7f5cc2997242ab730b469d
Mike Street <mike@liquidlight.co.uk>
2015-05-07 15:59:17 +0100

    Update styling and holding page content

---
+++ html/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Spark</title>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="/assets/spark/css/screen.css">
+</head>
+<body>
+	<main>
+		<section>
+			<h1>Spark Conference, Brighton</h1>
+			<p class="featured">Spark is a cheap, web-industry focused mini-conference hosted in the heart of Brighton.</p>
+		</section>
+		<div class="divider">
+			<section>
+				<p>To be kept up-to-date with the conference - sign up to the mailing list below, or follow us on <a href="http://www.twitter.com/sparkbrighton">Twitter</a>.</p>
+				<form action="signup-process.php" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" novalidate>
+					<div class="error msg js-error"></div>
+					<span class="field">
+						<input type="text" name="name" class="required name" id="name" />
+						<label for="name">
+							<span>Name</span>
+						</label>
+					</span>
+					<span class="field">
+						<input type="email" name="email" class="required email" id="email" />
+						<label for="email">
+							<span>Email Address</span>
+						</label>
+					</span>
+					<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+				</form>
+			</section>
+		</div>
+		<section>
+			<p>The evening event will feature four keynotes from people on the web-industry factory floor. Ranging from design to production, from tip and tricks to lessons in life. During the evening there will also be a chance to win some amazing prizes from some wonderful sponsors.</p>
+			<p>If you would like to sponsor the event by either donating some wonderful prizes or a contribution towards the running cost of the evening then please drop us an email: <a href="mailto:hello@sparkconf.uk">hello@sparkconf.uk</a>.</p>
+		</section>
+	</main>
+	<footer>
+		Spark - a mini-conference by <a href="http://www.liqudlight.co.uk">Liquid Light</a>
+	</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="assets/spark/js/core.js"></script>
+</body>
+</html>
