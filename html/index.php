bb247592cb8b413781c3ea814787106182d427b7
Mike Street <mike@liquidlight.co.uk>
2015-07-30 11:29:18 +0100

    Make it Byte. Make the website Byte with Bytes!

--- html/index.php
+++ html/index.php
@@ -4,27 +4,27 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Spark Conference Brighton - A mini-conference</title>
-	<meta name="description" content="Spark is an affordable, web-industry focused mini-conference hosted in the heart of Brighton raising money for local charities">
+	<title>Bytes Conference Brighton - A mini-conference on 29th October</title>
+	<meta name="description" content="Bytes is an affordable, web-industry focused mini-conference hosted in the heart of Brighton raising money for local charities">
 
-	<link rel="shortcut icon" href="/assets/spark/img/favicon.ico">
-	<link rel="apple-touch-icon" sizes="500x500" href="/assets/spark/img/spark-favicon.png">
+	<link rel="shortcut icon" href="/assets/bytes/img/favicon.ico">
+	<link rel="apple-touch-icon" sizes="500x500" href="/assets/bytes/img/bytes-favicon.png">
 
-	<meta itemprop="name" content="Spark Conference, Brighton">
-	<meta itemprop="description" content="Spark is an affordable, web-industry focused mini-conference hosted in the heart of Brighton raising money for local charities">
-	<meta itemprop="image" content="http://www.sparkconf.uk/assets/spark/img/spark-favicon.png">
+	<meta itemprop="name" content="Bytes Conference, Brighton">
+	<meta itemprop="description" content="Bytes is an affordable, web-industry focused mini-conference hosted in the heart of Brighton raising money for local charities">
+	<meta itemprop="image" content="http://www.bytesconf.uk/assets/bytes/img/bytes-favicon.png">
 
 	<meta name="twitter:card" content="summary">
-	<meta name="twitter:site" content="@sparkbrighton">
+	<meta name="twitter:site" content="@bytesconf">
 
-	<meta property="og:title" content="Spark Conference, Brighton"/>
+	<meta property="og:title" content="Bytes Conference, Brighton"/>
 	<meta property="og:type" content="website"/>
-	<meta property="og:url" content="http://www.sparkconf.uk" />
-	<meta property="og:image" content="http://www.sparkconf.uk/assets/spark/img/spark-favicon.png"/>
-	<meta property="og:description" content="Spark is an affordable, web-industry focused mini-conference hosted in the heart of Brighton raising money for local charities" />
-	<meta property="og:site_name" content="Spark Conference" />
+	<meta property="og:url" content="http://www.bytesconf.co.uk" />
+	<meta property="og:image" content="http://www.bytesconf.co.uk/assets/bytes/img/bytes-favicon.png"/>
+	<meta property="og:description" content="Bytes is an affordable, web-industry focused mini-conference hosted in the heart of Brighton raising money for local charities" />
+	<meta property="og:site_name" content="Bytes Conference" />
 
-	<link rel="stylesheet" href="/assets/spark/css/screen.css">
+	<link rel="stylesheet" href="/assets/bytes/css/screen.css">
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -39,16 +39,16 @@
 	<main>
 		<header>
 			<section>
-				<h1 class="logo"><img src="/assets/spark/img/logo.png" alt="Spark Conference, Brighton" /></h1>
+				<h1 class="logo"><img src="/assets/bytes/img/logo.png" alt="Bytes Conference, Brighton" /></h1>
 				<p class="featured">Thursday 29th October, 7pm - 11pm<br>Lighthouse, Kensington Street, Brighton, BN1 4AJ</p>
 				<p class="featured featured-sub">An affordable, web-industry focused mini-conference raising money for charities.</p>
-				<a href="Spark_Conf.ics" class="button">Add Spark to your calendar</a>
+				<a href="Bytes_Conf.ics" class="button">Add Bytes to your calendar</a>
 			</section>
 		</header>
 		<section>
-			<h2>What is Spark?</h2>
+			<h2>What is Bytes?</h2>
 			<div class="intro">
-				<p>Spark is a web-industry focused mini-conference to be held at <b>Lighthouse</b>, a hub for creative industries in the centre of Brighton, on <b>Thursday 29th October</b>.</p>
+				<p>Bytes is a web-industry focused mini-conference to be held at <b>Lighthouse</b>, a hub for creative industries in the centre of Brighton, on <b>Thursday 29th October</b>.</p>
 				<p>If you are just starting out, or an industry professional this affordable mini-conf will help you gain insight from our highly experienced key note speakers, with topics ranging from development and design to workflows and performance tips.</p>
 				<p>Every penny raised will be divided between our two charities - <b>abandofbrothers</b> and <b>drop4drop</b>. By attenting this mini-conf you are helping these great causes.</p>
 				<p>There will also be a chance for you to win some excellent swag - make sure you bring some extra pennies to be in with a chance of walking away with some prizes.</p>
@@ -84,7 +84,7 @@
 					<li>
 						<div class="block">
 							<a href="http://abandofbrothers.org.uk/">
-								<img src="/assets/spark/img/abob.png" alt="A Band of Brothers" />
+								<img src="/assets/bytes/img/abob.png" alt="A Band of Brothers" />
 							</a>
 							<div class="text">
 								<p>abandofbrothers is a charity established by men committed to positive social change through personal development and community building.</p>
@@ -96,7 +96,7 @@
 					<li>
 						<div class="block">
 							<a href="http://drop4drop.org/">
-								<img src="/assets/spark/img/drop4drop.png" alt="drop4drop" />
+								<img src="/assets/bytes/img/drop4drop.png" alt="drop4drop" />
 							</a>
 							<div class="text">
 								<p>Established in 2006, drop4drop is an independent charity on a mission to help alleviate the global water crisis, by working with both individuals and businesses to deliver clean water projects to developing communities around the world. </p>
@@ -115,7 +115,7 @@
 					<div class="block">
 						<div class="top">
 							<div class="cell">
-								<img src="/assets/spark/img/speakers/dan-edwards.jpg" alt="Dan Edwards" />
+								<img src="/assets/bytes/img/speakers/dan-edwards.jpg" alt="Dan Edwards" />
 							</div>
 							<div class="cell">
 								<h3>Dan Edwards</h3>
@@ -134,7 +134,7 @@
 					<div class="block">
 						<div class="top">
 							<div class="cell">
-								<img src="/assets/spark/img/speakers/rosa-fox.jpg" alt="Rosa Fox" />
+								<img src="/assets/bytes/img/speakers/rosa-fox.jpg" alt="Rosa Fox" />
 							</div>
 							<div class="cell">
 								<h3>Rosa Fox</h3>
@@ -153,7 +153,7 @@
 					<div class="block">
 						<div class="top">
 							<div class="cell">
-								<img src="/assets/spark/img/speakers/charlotte-jackson.jpg" alt="Charlotte Jackson" />
+								<img src="/assets/bytes/img/speakers/charlotte-jackson.jpg" alt="Charlotte Jackson" />
 							</div>
 							<div class="cell">
 								<h3>Charlotte Jackson</h3>
@@ -173,7 +173,7 @@
 					<div class="block">
 						<div class="top">
 							<div class="cell">
-								<img src="/assets/spark/img/speakers/will-chidlow.jpg" alt="Will Chidlow" />
+								<img src="/assets/bytes/img/speakers/will-chidlow.jpg" alt="Will Chidlow" />
 							</div>
 							<div class="cell">
 								<h3>Will Chidlow</h3>
@@ -194,41 +194,44 @@
 		<div id="map"></div>
 		<section>
 			<h2>With thanks to...</h2>
-			<ul class="set small">
+			<ul class="partners">
 				<li>
-					<a href="http://bisonbeer.co.uk/"><img src="/assets/spark/img/partners/bison-beer.jpg" alt="Bison Beer"></a>
+					<a rel="nofollow" href="http://bisonbeer.co.uk/"><img src="/assets/bytes/img/partners/bison-beer.jpg" alt="Bison Beer"></a>
 				</li>
 				<li>
-					<a href="http://www.drinkinbrighton.co.uk/deadwaxsocial"><img src="/assets/spark/img/partners/deadwax.jpg" alt="Dead Wax Social"></a>
+					<a rel="nofollow" href="http://www.bohogelato.co.uk/"><img src="/assets/bytes/img/partners/boho-gelato.jpg" alt="Boho Gelato"></a>
 				</li>
 				<li>
-					<a href="http://www.life-water.co.uk/">Lifewater</a>
+					<a rel="nofollow" href="http://www.drinkinbrighton.co.uk/deadwaxsocial"><img src="/assets/bytes/img/partners/deadwax.jpg" alt="Dead Wax Social"></a>
+				</li>
+				<li>
+					<a rel="nofollow" href="http://www.life-water.co.uk/"><img src="/assets/bytes/img/partners/lifewater.jpg" alt="Lifewater"></a>
 				</li>
 
 			</ul>
-			<ul class="set small">
+			<ul class="partners">
 				<li>
-					<a href="http://bluebirdteaco.com/"><img src="/assets/spark/img/partners/bluebird.jpg" alt="Bluebird Tea"></a>
+					<a rel="nofollow" href="http://bluebirdteaco.com/"><img src="/assets/bytes/img/partners/bluebird.jpg" alt="Bluebird Tea"></a>
 				</li>
 				<li>
-					<a href="http://www.cybercandy.co.uk/">Cybercandy</a>
+					<a rel="nofollow" href="http://www.cybercandy.co.uk/"><img src="/assets/bytes/img/partners/cybercandy.jpg" alt="Cybercandy"></a>
 				</li>
 				<li>
-					<a href="http://www.liquidlight.co.uk/">Liquid Light</a>
+					<a rel="nofollow" href="http://www.liquidlight.co.uk/"><img src="/assets/bytes/img/partners/liquid-light.jpg" alt="Liquid Light"></a>
 				</li>
 				<li>
-					<a href="http://www.komedia.co.uk/brighton/"><img src="/assets/spark/img/partners/komedia.jpg" alt="Komedia"></a>
+					<a rel="nofollow" href="http://www.komedia.co.uk/brighton/"><img src="/assets/bytes/img/partners/komedia.jpg" alt="Komedia"></a>
 				</li>
 			</ul>
 		</section>
 	</main>
 	<footer>
-		Spark - a mini-conference by <a href="http://www.liquidlight.co.uk">Liquid Light</a><br>
-		<a href="mailto:hello@sparkconf.uk">Email Us</a>&nbsp; | &nbsp;<a href="http://www.twitter.com/sparkbrighton">Tweet Us</a>
+		Bytes - a mini-conference by <a href="http://www.liquidlight.co.uk">Liquid Light</a><br>
+		<a href="mailto:hello@bytesconf.co.uk">Email Us</a>&nbsp; | &nbsp;<a href="http://www.twitter.com/bytesconf">Tweet Us</a>
 	</footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script src="assets/spark/js/core.js"></script>
+<script src="assets/bytes/js/core.js"></script>
 </body>
 </html>
