44c19f3ea54df59545426c217c857883ce21fc8c
Mike Street <mike@liquidlight.co.uk>
2016-05-27 10:58:00 +0100

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,1 @@
-require ('../../../../../usr/local/share/typo3/6.0/typo3/ext/ll_fe/Default/Build/gulp/gulpfile.js');
+require ('../../../../../usr/local/share/typo3/6.0/typo3/ext/ll_fe/Default/Build/gulp/gulpfile-v2.js');
