c13adbb6d90c28ed2c619b8db82bb4308db2c873
Joao Augusto <joao@liquidlight.co.uk>
2018-10-18 12:22:36 +0100

    FE: Set structure of the site

--- build/bytes-2019/js/core.js
+++ build/bytes-2019/js/core.js
@@ -1,1 +1,120 @@
-console.log('👋');
+// 'use strict';
+
+const triggers = document.querySelectorAll('.speakerTrigger'),
+	panels = document.querySelectorAll('.speakerPanelInner'),
+	panelContent = document.querySelector('.jsSpeakerDetails'),
+	scheduleLinks = document.querySelectorAll('.jsLinkToSpeaker');
+
+
+/**
+ * Injects speaker content in panel
+ * @param {Object} panel - Speaker content
+ */
+function injectContent(panel) {
+	panelContent.innerHTML = panel.innerHTML;
+}
+
+/**
+ * Update URL
+ * @param {String} title
+ * @param {String} url
+ */
+function updateURL(title, url) {
+	history.pushState({}, title, url);
+}
+
+/**
+ * Get the content from cached panels
+ * @param {String} speaker name
+ * @param {Boolean} update Wether it should update URL or not
+ */
+function getContent(name, update = true) {
+	for (const panel of panels) {
+		const panelName = panel.dataset.speakerContent;
+
+		if (name === panelName) {
+			injectContent(panel);
+
+			if (update) {
+				updateURL('Speakers', `/#${name}`);
+			}
+
+			return;
+		}
+	}
+}
+
+/**
+ * Smooth scroll
+ * @param {Object} element - scrolling to element
+ */
+function scrollIt(element) {
+	window.scrollTo({
+		'behavior': 'smooth',
+		'left': 0,
+		'top': element.offsetTop
+	});
+}
+
+function setActive(el) {
+
+	for (const speakerTrigger of triggers) {
+		speakerTrigger.classList.remove('isActive');
+	}
+
+	el.classList.add('isActive');
+}
+
+/**
+ * Click on speakers grid
+ */
+for(const speakerTrigger of triggers) {
+
+	speakerTrigger.addEventListener('click', function() {
+		const name = speakerTrigger.id;
+
+		if (name === 'undefined') {
+			console.warn('data-speaker-name not set!');
+			return;
+		}
+
+		setActive(speakerTrigger);
+		getContent(name);
+	});
+}
+
+/**
+ * Click on speakers on schedule
+ */
+for (const link of scheduleLinks) {
+	link.addEventListener('click', e => {
+		e.preventDefault();
+		const speaker = link.dataset.speaker,
+			el = document.querySelector(`.speakerTrigger[data-speaker-name='${speaker}']`);
+
+		getContent(speaker);
+		setActive(el);
+		scrollIt(el);
+	});
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	// Set first speaker as active
+	const speaker = triggers[0].id;
+	setActive(triggers[0]);
+	getContent(speaker, false);
+
+	// Scroll to speaker if exists
+	for (const speaker of triggers) {
+		if (window.location.hash.indexOf(speaker.id) > -1) {
+			const el = document.getElementById(speaker.id);
+			if (el) {
+				getContent(speaker.id);
+				setActive(el);
+				scrollIt(el);
+			}
+		}
+	}
+});
--- html/index.php
+++ html/index.php
@@ -7,11 +7,12 @@
 	<main>
 		<header>
 			<div class="head">
-				<svg xmlns="http://www.w3.org/2000/svg" width="657" height="201">
+				<svg xmlns="http://www.w3.org/2000/svg" width="219" height="67" class="logo">
 					<g fill="none" fill-rule="evenodd">
-						<path fill="#ff3c82" d="M88 51.6L507.2 0 657 151c-249.7 27.7-456.7 44.4-621 50z"/>
-						<path fill="#fff" d="M41.4 18h554L633 179.4c-260.9 1.6-471.9 0-633-5.2z"/>
-						<path fill="#2f1847" d="M140 126c-2.6 0-5-.5-7.4-1.4-2.4-1-4.3-2.4-5.9-4.3v4.7h-11.8V58.8h11.8v24c1.6-1.9 3.5-3.3 5.8-4.2a20 20 0 0 1 18.3 1.9c3 2.2 5.4 5.1 7 8.9 1.5 3.7 2.3 7.7 2.3 12.2 0 4.4-.8 8.4-2.3 12.2a20.8 20.8 0 0 1-7 8.8c-3 2.2-6.5 3.3-10.7 3.3zm-2.8-10.6c3.8 0 6.6-1.3 8.5-3.8 2-2.6 2.9-6 2.9-10 0-4.2-1-7.5-2.9-10.1-2-2.6-4.7-4-8.5-4s-6.6 1.3-8.6 4c-2 2.5-3 6-3 10 0 4.2 1 7.5 3 10 2 2.6 4.8 3.9 8.6 3.9zm40.4 8.3l-18.4-45.5h12.6l11.8 32.5 12-32.5h12.6l-26.8 67H169zm53.6 2.2c-3 0-5.6-.5-7.8-1.7s-3.9-2.7-5-4.8c-1.2-2-1.8-4.5-1.8-7.3V87.7H209v-9.5h7.8V66.5l11.2-8.1v19.8h11.7v9.5H228V110c0 2.2.5 3.7 1.4 4.5 1 1 2.2 1.4 3.8 1.4 1.1 0 2.2-.2 3.4-.5a18 18 0 0 0 3.1-1.2v10.3a26.3 26.3 0 0 1-8.5 1.4zm36.8 0a21.9 21.9 0 0 1-21-12.2 28.9 28.9 0 0 1-.1-24 21.2 21.2 0 0 1 20.4-12.5c5 0 9.3 1.2 12.5 3.5 3.3 2.4 5.6 5.4 7 9a30.5 30.5 0 0 1 1.7 15.6h-32.1c1 6.9 5 10.3 11.7 10.3 2.2 0 4.3-.4 6.1-1.2s3.6-2 5.2-3.8l7.8 6.9c-2.5 2.8-5.4 5-8.5 6.4-3.2 1.3-6.7 2-10.7 2zm9.7-28.8c0-1.2-.1-2.1-.4-3-.5-2.3-1.7-4.1-3.5-5.4s-4-1.9-6.6-1.9c-6 0-9.6 3.4-10.8 10.3zm33.5 28.8a22 22 0 0 1-15.6-6.5c-1.9-2-3.2-4.1-4-6.6l9.1-4c.8 2.2 2 4 3.5 5.5a9 9 0 0 0 6.5 2.2c1.6 0 3-.3 4.2-.9s2-1.2 2.5-2.1c.6-.9.9-1.7.9-2.6 0-1-.4-1.8-1.1-2.5s-1.7-1.2-2.8-1.6a58 58 0 0 0-4.7-1.3c-3.1-.9-5.6-1.7-7.6-2.5s-3.7-2.2-5.2-4a11.2 11.2 0 0 1-2.2-7.2 13.2 13.2 0 0 1 8.8-13c2.6-1 5.5-1.6 8.6-1.6A19.7 19.7 0 0 1 330 87.4l-8.9 4.8c-1-1.8-2.1-3.2-3.6-4.3s-3.3-1.7-5.6-1.7c-1.9 0-3.3.4-4.4 1.2-1.1.8-1.7 1.7-1.7 2.9 0 .8.4 1.6 1 2.2.7.6 1.6 1 2.6 1.4l4.5 1.4c3.2 1 5.8 1.9 8 2.8a12.7 12.7 0 0 1 7.8 12.2 14 14 0 0 1-2.7 8.7c-1.8 2.3-4 4-6.8 5.2a23.1 23.1 0 0 1-8.9 1.7zm46-.2a19 19 0 0 1-18-11.8 29.3 29.3 0 0 1-2.4-12.2c0-4.3.7-8.3 2.3-12s3.8-6.6 7-8.8 6.7-3.4 11-3.4a19 19 0 0 1 18.5 12.6l-4.3 1.6c-1.2-3.1-3.1-5.6-5.6-7.4s-5.3-2.7-8.5-2.7c-3.5 0-6.4 1-8.8 2.8s-4.2 4.2-5.4 7.3c-1.2 3-1.7 6.4-1.7 10 0 3.6.5 6.9 1.7 10 1.2 3 3 5.3 5.4 7.1 2.4 1.8 5.4 2.7 8.8 2.7 3.3 0 6.2-.9 8.6-2.6 2.4-1.8 4.2-4.2 5.2-7.4l4.1 1.5c-1.4 4-3.7 7.1-6.8 9.3s-6.8 3.4-11 3.4zm47.3 0a21 21 0 0 1-19.3-12 26.5 26.5 0 0 1-2.8-12.2c0-4.5 1-8.5 2.8-12.2 1.9-3.6 4.5-6.5 8-8.6 3.3-2.1 7-3.2 11.3-3.2A22 22 0 0 1 424 89.3c2 3.7 3 7.7 3 12.2 0 4.4-1 8.5-3 12.2s-4.5 6.6-8 8.7a21 21 0 0 1-11.4 3.3zm0-4c3.5 0 6.6-.9 9.3-2.7 2.7-1.7 4.8-4.2 6.2-7.3a24.7 24.7 0 0 0 0-20.4c-1.4-3-3.5-5.4-6.2-7.2a16.4 16.4 0 0 0-9.3-2.6 16.3 16.3 0 0 0-15.3 9.8c-1.5 3-2.2 6.4-2.2 10.2 0 3.8.7 7.2 2.2 10.3 1.4 3 3.5 5.5 6.2 7.2 2.6 1.8 5.7 2.7 9.1 2.7zm34.8-43.5h4.4v8.7c1.4-3 3.3-5.3 6-7s5.6-2.4 9-2.4c5 0 9 1.6 11.7 4.7 2.7 3.1 4.2 7.3 4.3 12.6V125h-4.4V96.3c0-4.7-1.1-8.3-3.3-10.8-2.1-2.6-5.3-3.8-9.4-3.8-2.8 0-5.2.6-7.3 1.7-2.1 1.2-3.7 3-4.8 5.2s-1.8 4.9-1.8 8V125h-4.4zm54 3.8h-10v-3.8h10.1v-7.6c0-4 1.2-7.2 3.4-9.3a13 13 0 0 1 9.3-3.2c2.1 0 4.3.6 6.5 1.7l-1.5 3.7a9.7 9.7 0 0 0-4.6-1.4c-5.7 0-8.6 3-8.6 8.9v7.2h14.7V82H498v43h-4.5zm26.9 33.4h4.5v9.6h-4.5z"/>
+						<path fill="#FF3C82" d="M29.3 17.2L169.1 0 219 50.3A2805 2805 0 0 1 12 67l17.3-49.8z"/>
+						<path fill="#FFF" d="M13.8 6h184.6L211 59.8c-87 .5-157.3 0-211-1.7L13.8 6z"/>
+						<path fill="#2F1847" d="M46.7 42.3c-.9 0-1.7-.1-2.5-.4-.8-.4-1.4-.8-2-1.5V42h-3.9V20h4v8c.5-.7 1.1-1.2 1.9-1.5.7-.3 1.5-.4 2.4-.4a6 6 0 0 1 3.7 1c1 .8 1.8 1.8 2.3 3a10.3 10.3 0 0 1 0 8.1 7 7 0 0 1-2.3 3 6 6 0 0 1-3.6 1.1zm-1-3.5c1.3 0 2.2-.4 2.9-1.3.6-.8 1-2 1-3.3 0-1.4-.4-2.5-1-3.4-.7-.8-1.6-1.3-2.9-1.3-1.2 0-2.2.4-2.8 1.3-.7.9-1 2-1 3.4s.3 2.5 1 3.3c.6.9 1.6 1.3 2.8 1.3zm13.5 2.8L53 26.4h4.2l4 10.8 4-10.8h4l-8.8 22.3h-4.2l2.9-7.1zm17.9.7c-1 0-2-.2-2.6-.5a4 4 0 0 1-1.7-1.7 5 5 0 0 1-.6-2.4v-8.1h-2.5v-3.2h2.6v-3.9l3.7-2.7v6.6h4v3.2h-4V37c0 .7.2 1.2.5 1.5.3.3.7.5 1.2.5l1.2-.2a6 6 0 0 0 1-.4v3.4c-.3.2-.7.3-1.3.4l-1.5.1zm12.2 0a8 8 0 0 1-4.3-1.1 7 7 0 0 1-2.7-3 9.6 9.6 0 0 1 0-8 7 7 0 0 1 6.8-4.1 7 7 0 0 1 4.2 1.1c1 .8 1.8 1.8 2.3 3a10 10 0 0 1 .6 5.2H85.5c.3 2.3 1.6 3.5 3.9 3.5.7 0 1.4-.2 2-.4a5 5 0 0 0 1.7-1.3l2.6 2.3c-.8 1-1.8 1.7-2.8 2.1-1 .5-2.2.7-3.6.7zm3.3-9.6c0-.4 0-.7-.2-1a3 3 0 0 0-1.1-1.8c-.7-.4-1.4-.6-2.2-.6-2 0-3.2 1.1-3.6 3.4h7zm11.1 9.6a7.3 7.3 0 0 1-5.2-2.2c-.6-.6-1-1.3-1.3-2.2l3-1.3c.3.7.7 1.3 1.2 1.8a3 3 0 0 0 2.2.8c.5 0 1-.1 1.4-.3l.8-.7.3-.9a1 1 0 0 0-.4-.8c-.2-.3-.5-.4-.9-.6l-1.6-.4a19 19 0 0 1-2.5-.8c-.7-.3-1.2-.8-1.7-1.4-.5-.6-.8-1.4-.8-2.4a4.4 4.4 0 0 1 3-4.3c.8-.4 1.8-.5 2.8-.5a6.6 6.6 0 0 1 6 3.4l-3 1.6c-.3-.6-.7-1.1-1.2-1.5a3 3 0 0 0-1.9-.5c-.6 0-1 .1-1.4.4-.4.2-.6.5-.6 1 0 .2.1.5.3.7l1 .4 1.4.5c1 .3 2 .6 2.6 1 .7.3 1.4.7 1.9 1.4s.7 1.5.7 2.6c0 1.2-.3 2.1-.8 2.9-.6.8-1.4 1.4-2.3 1.7-1 .4-2 .6-3 .6zm15.4 0a6.3 6.3 0 0 1-6-4 9.8 9.8 0 0 1-.8-4c0-1.5.2-2.8.7-4a7 7 0 0 1 2.3-3c1-.8 2.3-1.1 3.8-1.1a6.4 6.4 0 0 1 6 4.2l-1.3.5c-.4-1-1-1.9-1.9-2.5-.8-.6-1.8-.9-2.8-.9-1.2 0-2.2.3-3 1-.8.6-1.4 1.4-1.8 2.4a9 9 0 0 0-.5 3.3 9 9 0 0 0 .5 3.3c.4 1 1 1.8 1.8 2.4.8.6 1.8 1 3 1 1 0 2-.4 2.8-1 .8-.5 1.4-1.3 1.8-2.4l1.3.5a6.2 6.2 0 0 1-2.2 3.1c-1 .7-2.3 1.1-3.7 1.1zm15.7 0a7 7 0 0 1-6.4-4c-.6-1.3-1-2.6-1-4.1 0-1.5.4-2.9 1-4a7 7 0 0 1 6.4-4 7 7 0 0 1 6.5 4c.7 1.1 1 2.5 1 4 0 1.4-.3 2.8-1 4a7.4 7.4 0 0 1-2.6 3 7 7 0 0 1-3.9 1zm0-1.4c1.2 0 2.2-.3 3.1-.9a6 6 0 0 0 2.1-2.4c.5-1 .8-2.2.8-3.4 0-1.3-.3-2.4-.8-3.4a6 6 0 0 0-2-2.4c-1-.6-2-1-3.2-1a5.4 5.4 0 0 0-5 3.4c-.6 1-.8 2.1-.8 3.4a8 8 0 0 0 .7 3.4c.5 1 1.2 1.8 2 2.4 1 .6 2 1 3.1 1zm11.6-14.5h1.5v3a5 5 0 0 1 2-2.4c.9-.6 1.9-.8 3-.8 1.7 0 3 .5 3.9 1.5 1 1 1.4 2.5 1.4 4.2V42h-1.4v-9.6c0-1.5-.4-2.7-1.1-3.6-.7-.8-1.8-1.2-3.2-1.2-.9 0-1.7.2-2.4.5a4 4 0 0 0-1.6 1.8c-.4.7-.6 1.6-.6 2.6V42h-1.5V26.4zm18 1.3h-3.3v-1.3h3.4v-2.5c0-1.4.4-2.4 1.1-3.1a4.7 4.7 0 0 1 5.3-.5l-.5 1.2c-.6-.3-1-.5-1.6-.5-1.9 0-2.8 1-2.8 3v2.4h4.9v1.3h-5V42h-1.4V27.7z"/>
+						<circle cx="175.8" cy="40.8" r="1.8" fill="#2F1847"/>
 					</g>
 				</svg>
 
@@ -37,11 +38,266 @@
 			<a href="#" class="btn">Buy tickets</a>
 		</div>
 
+		<section class="sectionTextImage">
+			<div class="content">
+				<div class="contentInner">
+					<div class="shapeOutside"></div>
+					<h2>All proceeds support our industries community</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla, aut facere delectus temporibus minus ad cupiditate aliquam veritatis? Velit amet natus harum aliquid mollitia aut omnis tempore dolorum fugiat!</p>
+				</div>
+			</div>
+			<figure>
+				<img src="/assets/bytes-2019/img/placeholder.jpg" alt="Placeholder">
+			</figure>
+		</section>
+
+		<section class="sectionTextImage sectionTertiary alt">
+			<figure>
+				<img src="/assets/bytes-2019/img/placeholder.jpg" alt="Placeholder">
+			</figure>
+			<div class="content">
+				<div class="contentInner">
+					<div class="shapeOutside"></div>
+					<h2>Four forward-thinking talks</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla, aut facere delectus temporibus minus ad cupiditate aliquam veritatis? Velit amet natus harum aliquid mollitia aut omnis tempore dolorum fugiat!</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="sectionTextImage">
+			<div class="content">
+				<div class="contentInner">
+					<div class="shapeOutside"></div>
+					<h2>Laid back atmosphere & raffle!</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla, aut facere delectus temporibus minus ad cupiditate aliquam veritatis? Velit amet natus harum aliquid mollitia aut omnis tempore dolorum fugiat!</p>
+				</div>
+			</div>
+			<figure>
+				<img src="/assets/bytes-2019/img/placeholder.jpg" alt="Placeholder">
+			</figure>
+		</section>
+
+		<section class="callout">
+			<h2>If you are just starting out, or an industry professional, this affordable mini-conf will help you gain insight from our four keynote speakers.</h2>
+		</section>
+
+		<section id="speakers" class="speakers">
+			<dl class="speakersList">
+				<!-- Lisa Freeman -->
+				<dt class="speakerItem">
+					<div id="lisa-freeman" class="speakerTrigger">
+						<img src='http://placekitten.com/400/400' alt=''/>
+						<span>Lisa Freeman</span>
+					</div>
+				</dt>
+				<dd class="speakerPanel">
+					<div class="speakerPanelInner" data-speaker-content="lisa-freeman">
+						<div class="speakerContent">
+							<h2>Lisa Freeman</h2>
+							<h3>Regulation and privacy: what might be coming?</h3>
+							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus dolore distinctio odit ipsum culpa rerum, sequi tenetur, expedita beatae laboriosam perspiciatis harum eos obcaecati! Officiis ratione quo cum atque a!</p>
+
+							<ul>
+								<li><a href="#">@TwitterHandle</a></li>
+								<li><a href="#">www.heynicesite.com</a></li>
+							</ul>
+						</div>
+
+						<figure class="speakerImage">
+							<img src='http://placekitten.com/500/500' alt=''/>
+						</figure>
+					</div>
+				</dd>
+
+				<!-- Matt Keogh -->
+				<dt class="speakerItem">
+					<div id="matt-keogh" class="speakerTrigger">
+						<img src='http://placekitten.com/410/410' alt=''/>
+						<span>Matt Keogh</span>
+					</div>
+				</dt>
+				<dd class="speakerPanel">
+					<div class="speakerPanelInner" data-speaker-content="matt-keogh">
+						<div class="speakerContent">
+							<h2>Matt Keogh</h2>
+							<h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus dolore distinctio odit ipsum culpa rerum, sequi tenetur, expedita beatae laboriosam perspiciatis harum eos obcaecati! Officiis ratione quo cum atque a!</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate consequatur porro dolor quod aut in. Repellat obcaecati praesentium voluptatem ea veritatis laborum. Modi laboriosam ipsum sapiente, molestias quasi nisi placeat.</p>
+
+							<ul>
+								<li><a href="#">@TwitterHandle</a></li>
+								<li><a href="#">www.heynicesite.com</a></li>
+							</ul>
+						</div>
+
+						<figure class="speakerImage">
+							<img src='http://placekitten.com/520/520' alt=''/>
+						</figure>
+					</div>
+				</dd>
+
+				<!-- Mike Street -->
+				<dt class="speakerItem">
+					<div id="mike-street" class="speakerTrigger">
+						<img src='http://placekitten.com/420/420' alt=''/>
+						<span>Mike Street</span>
+					</div>
+				</dt>
+				<dd class="speakerPanel">
+					<div class="speakerPanelInner" data-speaker-content="mike-street">
+						<div class="speakerContent">
+							<h2>Mike Street</h2>
+							<h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+							<p>Lorem ipsum amet consectetur adipisicing elit. Delectus dolore distinctio odit ipsum culpa rerum, sequi tenetur, expedita beatae laboriosam perspiciatis harum eos obcaecati! Officiis ratione quo cum atque a!</p>
+							<p>Lorem ipsum consequatur porro dolor quod aut in. Repellat obcaecati praesentium voluptatem ea veritatis laborum. Modi laboriosam ipsum sapiente, molestias quasi nisi placeat.</p>
+
+							<ul>
+								<li><a href="#">@TwitterHandle</a></li>
+								<li><a href="#">www.heynicesite.com</a></li>
+							</ul>
+						</div>
+
+						<figure class="speakerImage">
+							<img src='http://placekitten.com/530/530' alt=''/>
+						</figure>
+					</div>
+				</dd>
+
+				<!-- Candice Underwood -->
+				<dt class="speakerItem">
+					<div id="candice-underwood" class="speakerTrigger">
+						<img src='http://placekitten.com/425/425' alt=''/>
+						<span>Candice Underwood</span>
+					</div>
+				</dt>
+				<dd class="speakerPanel">
+					<div class="speakerPanelInner" data-speaker-content="candice-underwood">
+						<div class="speakerContent">
+							<h2>Candice Underwood</h2>
+							<h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+							<p>Lorem ipsum amet consectetur adipisicing elit. Delectus dolore distinctio odit ipsum culpa rerum, sequi tenetur, expedita beatae laboriosam perspiciatis harum eos obcaecati! Officiis ratione quo cum atque a!</p>
+							<p>Lorem ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam explicabo perferendis quibusdam, eaque at deleniti alias excepturi! Eveniet ducimus sunt aperiam nulla ratione voluptate, delectus rem recusandae iure reiciendis atque! consequatur porro dolor quod aut in. Repellat obcaecati praesentium voluptatem ea veritatis laborum. Modi laboriosam ipsum sapiente, molestias quasi nisi placeat.</p>
+
+							<ul>
+								<li><a href="#">@TwitterHandle</a></li>
+								<li><a href="#">www.heynicesite.com</a></li>
+							</ul>
+						</div>
+
+						<figure class="speakerImage">
+							<img src='http://placekitten.com/535/535' alt=''/>
+						</figure>
+					</div>
+				</dd>
+
+				<!-- Al Taylor -->
+				<dt class="speakerItem">
+					<div id="al-taylor" class="speakerTrigger">
+						<img src='http://placekitten.com/430/430' alt=''/>
+						<span>Al Taylor</span>
+					</div>
+				</dt>
+				<dd class="speakerPanel">
+					<div class="speakerPanelInner" data-speaker-content="al-taylor">
+						<div class="speakerContent">
+							<h2>Al Taylor</h2>
+							<h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+							<p>Lorem ipsum amet consectetur adipisi. Lectus dolore distinctio odit ipsum culpa rerum, sequi tenetur, expedita beatae laboriosam perspiciatis harum eos obcaecati! Officiis ratione quo cum atque a!</p>
+							<p>Lorem ipsr, adipisicing elit. Quam explicabo perferendis quibusdam, eaque at deleniti alias excepturi! Eveniet ducimus sunt aperiam nulla ratione voluptate, delectus rem recusandae iure reiciendis atque! consequatur porro dolor quod aut in. Repellat obcaecati praesentium voluptatem ea veritatis laborum. Modi laboriosam ipsum sapiente, molestias quasi nisi placeat.</p>
+
+							<ul>
+								<li><a href="#">@TwitterHandle</a></li>
+								<li><a href="#">www.heynicesite.com</a></li>
+							</ul>
+						</div>
+
+						<figure class="speakerImage">
+							<img src='http://placekitten.com/535/535' alt=''/>
+						</figure>
+					</div>
+				</dd>
+
+
+			</dl>
+
+			<div class="speakerDetails">
+				<div class="jsSpeakerDetails speakerPanelInner wrapper"></div>
+			</div>
+		</section>
+
+		<section id="schedule" class="schedule">
+			<div class="wrapperThin">
+				<h2>The schedule</h2>
+
+				<table>
+					<tr>
+						<td>7:00pm</td>
+						<td><p>Doors Open</p></td>
+					</tr>
+					<tr>
+						<td>7:30pm</td>
+						<td>
+							<h3><a href="#speakers" data-speaker="lisa-freeman" class="jsLinkToSpeaker">Lisa Freeman</a></h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit tenetur voluptatibus saepe placeat asperiores sequi incidunt. Nostrum tenetur ab dolores voluptatum consequuntur incidunt molestiae eos sapiente nobis consectetur? Sed, impedit?</p>
+						</td>
+					</tr>
+					<tr>
+						<td>8:15pm</td>
+						<td>
+							<p>Break</p>
+						</td>
+					</tr>
+					<tr>
+						<td>8:30pm</td>
+						<td>
+							<h3><a href="#speakers" data-speaker="matt-keogh" class="jsLinkToSpeaker">Matt Keogh</a></h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora amet deserunt veniam quos saepe a minima facere. Repudiandae odit recusandae veritatis minima quis.</p>
+						</td>
+					</tr>
+					<tr>
+						<td>9:00pm</td>
+						<td>
+							<h3><a href="#">Mike Street</a></h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora amet deserunt veniam quos saepe a minima facere. Repudiandae odit recusandae veritatis minima quis.</p>
+						</td>
+					</tr>
+					<tr>
+						<td>9:30pm</td>
+						<td>
+							<p>Break</p>
+						</td>
+					</tr>
+					<tr>
+						<td>9:45pm</td>
+						<td>
+							<h3><a href="#">Graeme Robinson</a></h3>
+							<p>Autism and the Web</p>
+						</td>
+					</tr>
+					<tr>
+						<td>10:15pm</td>
+						<td>
+							<p>Raffle</p>
+						</td>
+					</tr>
+					<tr>
+						<td>10:45pm</td>
+						<td>
+							<p>Break</p>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</section>
+
+		<section id="map" class="maps">
+			<h2>Where to find us</h2>
+			<img src='https://placehold.it/1600x500' alt=''/>
+		</section>
+
+
 		<section id="partners">
 			<h2>With thanks to...</h2>
-			<ul class="partners partnersPlatinum">
-
-			</ul>
 			<ul class="partners partnersLower">
 				<li>
 					<a rel="nofollow" href="https://www.oreilly.com/"><img src="/assets/bytes/img/partners/orm.jpg" alt="O'Reilly"></a>
