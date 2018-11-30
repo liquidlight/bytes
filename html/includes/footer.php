7c32cec03005e7237d7b3f787592e51655118478
Mike Street <mike@liquidlight.co.uk>
2018-11-30 17:17:24 +0000

    FE: Add code of conduct page

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<p>Bytes - a mini-conference by <a href="https://www.liquidlight.co.uk">Liquid Light</a>. on <?php echo $date ?></p>
 	<div class="links">
-		<a href="mailto:hello@bytesconf.co.uk">Email Us</a><a href="https://www.twitter.com/bytesconf">Tweet Us</a><br>
+		<a href="/code-of-conduct.php">Code of Conduct</a><a href="mailto:hello@bytesconf.co.uk">Email Us</a><a href="https://www.twitter.com/bytesconf">Tweet Us</a><br>
 	</div>
 	<div class="links">
 		<a href="http://2017.bytesconf.co.uk">Bytes in 2017</a><a href="http://2016.bytesconf.co.uk">Bytes in 2016</a><a href="http://2015.bytesconf.co.uk">Bytes in 2015</a>
