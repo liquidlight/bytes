a3c6ca0b34aab7da31480eba558d8d7d195a3898
Mike Street <mike@liquidlight.co.uk>
2020-01-13 09:28:47 +0000

    Chore: Update gulpfile to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 global.config = {
-	'flags': {
-		'hasBabel': true,
-		'hasPostcssVariablesEnabled': false,
-		'hasPostcssCalcEnabled': false
+	flags: {
+		hasBabel: true,
+		hasPostcssVariablesEnabled: false,
+		hasPostcssCalcEnabled: false
 	}
 };
 
-require ('../../../../../usr/local/share/typo3/8.7/typo3/ext/ll_fe/Default/Build/gulp/gulpfile.js');
+module.exports = require('/usr/local/share/typo3/latest/typo3/ext/ll_fe/Default/Build/gulp4');
